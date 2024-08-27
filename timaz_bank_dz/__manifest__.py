ece41fb0a7b01ef85a7188530977b123efdb973c
Messadi Fatima <tima.messadi.04@gmail.com>
2024-08-27 21:02:28 +0100

    Add data file

---
+++ timaz_bank_dz/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': "Banques - Algérie",
+    'summary': """ Liste des Banques Algériennes """,
+    'description': """ Liste de Banques Algériennes """,
+
+
+    'category': 'service',
+    'version': '17.0.1.0',
+
+    'sequence': 1,
+
+
+    'author': "Timaz",
+    'website': "",
+
+
+    "license": "LGPL-3",
+    "price": 9.0,
+    "currency": 'EUR',
+
+    
+    'depends': ['base', 'contacts'],
+    
+    'data': [
+             
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
